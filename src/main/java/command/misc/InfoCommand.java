52845d8213af09d75791ca9f93f3b6052e884c92
Pascal <pascalwar@gmail.com>
2022-09-05 00:33:15 +0200

    tmp

--- src/main/java/command/misc/InfoCommand.java
+++ src/main/java/command/misc/InfoCommand.java
@@ -1,10 +1,10 @@
 package command.misc;
 
 import command.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.utils.FileUpload;
-import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 import java.io.File;
 
@@ -17,9 +17,11 @@ public class InfoCommand extends Command {
     @Override
     public void handle(MessageReceivedEvent event, String... params) {
         TextChannel channel = event.getChannel().asTextChannel();
-        MessageCreateBuilder builder = new MessageCreateBuilder();
-        builder.addFiles(FileUpload.fromData(new File("jeremy.png")));
-        channel.sendMessage(builder.build()).queue();
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setImage("attachment://jeremy.png") // we specify this in sendFile as "cat.png"
+                .setDescription("This is a cute cat :3");
+        channel.sendFiles(FileUpload.fromData(new File("jeremy.png"), "jeremy.png"))
+                .setEmbeds(embed.build()).queue();
     }
 
 
